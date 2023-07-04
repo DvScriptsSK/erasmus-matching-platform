a5833029391f0cc1408085541cf67e5481ba5326
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-04 21:30:54 +0200

    start of project list

--- src/components/project-list/ProjectList.jsx
+++ src/components/project-list/ProjectList.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import projectsData from '../../projectsData';
-import './project.css';  
+import './project.css';
+
 const ProjectList = () => {
   return (
-    <div>
-      <h2>Project List</h2>
-      {projectsData.map((project) => (
-        <div key={project.id} className='project-list'>
-          <h3>
-            <Link to={`/project/${project.id}`}>{project.title}</Link>
-          </h3>
-          <p>{project.description}</p>
-        </div>
-      ))}
+    <div className='project-list-container-div'>
+      <h2 className='project-list-container-title'>ALL PROJECTS</h2>
+      <center>
+      <div className='project-list-container'>
+        {projectsData.map((project) => (
+            <div className='project-list'>
+          <Link to={`/project/${project.id}`} key={project.id} style={{ textDecoration: 'none' }}>
+              <h3 className='project-list-des'>
+                <span className='project-list-title'>{project.title}</span>
+              </h3>
+              <span className='project-list-organizer'>{project.organizer}</span>
+              <p className='project-list-desc'>{project.description}</p>
+              </Link>
+            </div>
+        ))}
+      </div>
+      </center>
     </div>
   );
 };
--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -1,8 +1,26 @@
 const projectsData = [
-    { id: 1, title: 'Project 1', description: 'Lorem ipsum dolor sit amet.' },
-    { id: 2, title: 'Project 2', description: 'Consectetur adipiscing elit.' },
-    { id: 3, title: 'Project 3', description: 'Praesent ac orci ut nisl faucibus tempus.' },
-  ];
+  { id: 1, title: 'Project 1', description: 'Lorem ipsum dolor sit amet.', organizer: 'Organizer A' },
+  { id: 2, title: 'Project 2', description: 'Consectetur adipiscing elit.', organizer: 'Organizer B' },
+  { id: 3, title: 'Project 3', description: 'Praesent ac orci ut nisl faucibus tempus.', organizer: 'Organizer C' },
+  { id: 4, title: 'Project 4', description: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.', organizer: 'Organizer D' },
+  { id: 5, title: 'Project 5', description: 'Nulla tristique tellus ut libero venenatis, et convallis lectus varius.', organizer: 'Organizer E' },
+  { id: 6, title: 'Project 6', description: 'Sed commodo nulla vitae erat accumsan efficitur.', organizer: 'Organizer F' },
+  { id: 7, title: 'Project 7', description: 'Cras tempor lectus nec massa posuere rutrum.', organizer: 'Organizer G' },
+  { id: 8, title: 'Project 8', description: 'Fusce eu mauris condimentum, suscipit odio sed, gravida nisl.', organizer: 'Organizer H' },
+  { id: 9, title: 'Project 9', description: 'Vivamus id erat sollicitudin, eleifend orci at, sollicitudin justo.', organizer: 'Organizer I' },
+  { id: 10, title: 'Project 10', description: 'Maecenas cursus turpis id aliquet dignissim.', organizer: 'Organizer J' },
+  { id: 11, title: 'Project 11', description: 'Proin consectetur lacus sit amet risus dignissim rhoncus.', organizer: 'Organizer K' },
+  { id: 12, title: 'Project 12', description: 'Vivamus aliquet mi vel enim hendrerit, nec venenatis ipsum commodo.', organizer: 'Organizer L' },
+  { id: 13, title: 'Project 13', description: 'Duis nec nisi in massa varius interdum vel sed lectus.', organizer: 'Organizer M' },
+  { id: 14, title: 'Project 14', description: 'Pellentesque dapibus lectus quis dolor feugiat, sed placerat est rutrum.', organizer: 'Organizer N' },
+  { id: 15, title: 'Project 15', description: 'Suspendisse tristique lectus sed neque facilisis, id finibus purus tempor.', organizer: 'Organizer O' },
+  { id: 16, title: 'Project 16', description: 'Vestibulum rhoncus justo ut mauris dignissim interdum.', organizer: 'Organizer P' },
+  { id: 17, title: 'Project 17', description: 'Curabitur euismod est non risus vulputate, non sollicitudin quam finibus.', organizer: 'Organizer Q' },
+  { id: 18, title: 'Project 18', description: 'Fusce ullamcorper leo vitae felis gravida, ac dignissim turpis sagittis.', organizer: 'Organizer R' },
+  { id: 19, title: 'Project 19', description: 'Phasellus vestibulum dui eu sapien laoreet, eget convallis sem eleifend.', organizer: 'Organizer S' },
+  { id: 20, title: 'Project 20', description: 'Ut consequat erat sit amet ultrices malesuada.', organizer: 'Organizer T' }
+];
+
   
   export default projectsData;
   
