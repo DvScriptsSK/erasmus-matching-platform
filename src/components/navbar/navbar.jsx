13b3035aa8b0aebe571402a52f53d4405d81f2b1
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-07 10:03:50 +0200

    navbar logo and css

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,8 +5,13 @@ const NavBar = () => {
   return (
     <div className='navbar'>
       <div className='navbar__logo'>
-        <img src='https://i.ibb.co/C7KnLbq/Logo.png' alt='logo' />
+        <img src='https://media.discordapp.net/attachments/845988671538331649/1126784398797328405/Logo.png' alt='logo' />
       </div>
+      <div className='navbar__links'>
+        <a href='#'>Home</a>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+     </div>
     </div>
   );
 };
