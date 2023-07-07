9c38adb5c9bf9d5b8e5640d05f80175ec11dd49a
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-07 09:53:48 +0200

    navbar start

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.css';
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar__logo'>
+        <img src='https://i.ibb.co/C7KnLbq/Logo.png' alt='logo' />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
