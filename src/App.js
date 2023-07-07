35f7b00762e43b05bb62d1e09557a1b7774b4330
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-07 11:04:43 +0200

    optimize code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ProjectList from './components/project-list/ProjectList';
-import './App.css';
-import projectsData from './projectsData';
-import NavBar from './components/navbar/navbar';
+  import React from 'react';
+  import ProjectList from './components/project-list/ProjectList';
+  import './App.css';
+  import projectsData from './projectsData';
+  import NavBar from './components/navbar/navbar';
 
-const App = () => {
-  return (
-    <div>
-      <NavBar />
-      <div className='main'>
-        <div className='header'>
-          <h1>Erasmus Projects</h1>
-          <h2>Find projects that suit you</h2>
-          <button type="button">Find project</button>
+  const App = () => {
+    return (
+      <div>
+        <NavBar />
+        <div className='main'>
+          <div className='header'>
+            <h1>Erasmus Projects</h1>
+            <h2>Find projects that suit you</h2>
+            <button type="button">Find project</button>
 
+          </div>
+            <div className='listside'>
+              <ProjectList projects={projectsData} />
+            </div>
         </div>
-        <ProjectList projects={projectsData} />
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default App;
+  export default App;
