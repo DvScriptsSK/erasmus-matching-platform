884b62fa0726c358d55947fdd631474ce37cfc1c
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-07 13:43:09 +0200

    details new page

--- src/components/project-detail/ProjectDetails.jsx
+++ src/components/project-detail/ProjectDetails.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import projectsData from '../../projectsData';
+import NavBar from '../navbar/navbar';
+import './projectdetail.css';
 
 const ProjectDetails = () => {
   const { id } = useParams();
+  console.log(id);
 
-  // Find the project with the matching ID
   const project = projectsData.find((project) => project.id === parseInt(id));
+
   console.log(project);
   if (!project) {
     return <div>Project not found</div>;
@@ -14,8 +17,31 @@ const ProjectDetails = () => {
 
   return (
     <div>
-      <h2>{project.title}</h2>
-      <p>{project.description}</p>
+      <NavBar />
+      <center>
+        <div className='project-detail-container'>
+          <h2>{project.title}</h2>
+          <p>{project.description}</p>
+          <p>{project.organizer}</p>
+          <div className='project-detail-buttons'>
+            <button className='project-detail-button'>Info Pack</button>
+            <button className='project-detail-button'>Register</button>
+          </div>
+          <div className='project-detail-map'>
+            {/* Include map component or embed map iframe */}
+            <p>Location Map</p>
+          </div>
+          <div className='project-detail-requirements'>
+            <h3>Requirements</h3>
+            <ul>
+              <li>Requirement 1</li>
+              <li>Requirement 2</li>
+              <li>Requirement 3</li>
+              {/* Add more requirements as needed */}
+            </ul>
+          </div>
+        </div>
+      </center>
     </div>
   );
 };
