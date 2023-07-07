881a106cb93b74a64b0160664928d946581fbb18
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-07 13:43:01 +0200

    fixing listing

--- src/components/project-list/ProjectList.jsx
+++ src/components/project-list/ProjectList.jsx
@@ -8,20 +8,20 @@ const ProjectList = () => {
     <div className='project-list-container-div'>
       <h2 className='project-list-container-title'>ALL PROJECTS</h2>
       <center>
-      <div className='project-list-container'>
-        {projectsData.map((project) => (
-            <div className='project-list'>
-            <img className='project-list-img' src='https://www.adelslovakia.org/wp-content/uploads/2023/05/The-Europe-of-Language.jpg' alt={project.title} />
+        <div className='project-list-container'>
+          {projectsData.map((project) => (
+            <div className='project-list' key={project.id}>
+              <img className='project-list-img' src='https://www.adelslovakia.org/wp-content/uploads/2023/05/The-Europe-of-Language.jpg' alt={project.title} />
               <h3 className='project-list-des'>
                 <span className='project-list-title'>{project.title}</span>
               </h3>
               <span className='project-list-organizer'>{project.organizer}</span>
-              <Link to={`/project/${project.id}`} key={project.id} style={{ textDecoration: 'none' }}>
-              <button className='project-list-btn'>View Project</button>
+              <Link to={`/project/${project.id}`} style={{ textDecoration: 'none' }}>
+                <button className='project-list-btn'>View Project</button>
               </Link>
             </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </center>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from './components/auth/login';
+import ProjectDetails from './components/project-detail/ProjectDetails';
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path='/auth/login' element={<Login />} />
+      <Route path='/auth/login' element={<Login/>} />
+      <Route path="/project/:id" element={<ProjectDetails/>} />
     </Routes>
   </Router>,
   document.getElementById('root')
