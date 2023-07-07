ee22c47e56de97e7efaed9d18124e245ced40069
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-07 09:54:04 +0200

    colour scheme responsive v1

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ProjectList from './components/project-list/ProjectList';
 import './App.css';
 import projectsData from './projectsData';
+import NavBar from './components/navbar/navbar';
 
 const App = () => {
   return (
+    <div>
+      <NavBar />
       <div className='main'>
         <div className='header'>
           <h1>Erasmus Projects</h1>
           <h2>Find projects that suit you</h2>
           <button type="button">Find project</button>
         </div>
-          <ProjectList projects={projectsData} />
-        </div>
+        <ProjectList projects={projectsData} />
+      </div>
+    </div>
   );
 };
-
 
 export default App;
