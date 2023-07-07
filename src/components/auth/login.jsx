28088efc2032fb1448ede4886b5324aca2a8c57b
dv-development <72212575+DvScriptsSK@users.noreply.github.com>
2023-07-07 13:18:07 +0200

    nabar and login finish demo

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -11,10 +11,11 @@ const Login = () => {
             <h1>Sign in</h1>
             <form>
             <h5>E-mail</h5>
-            <input type="email" />
+            <input type="email" placeholder='erasmusproject@email.com'/>
             <h5>Password</h5>
-            <input type="password" />
+            <input type="password" placeholder='Enter Password' />
             <button type="submit" className="login__signInButton">Sign In</button>
+            <a href="/auth/register"><button className="login__registerButton">Create your Account</button></a>
             </form>
         </div>
         </div>
